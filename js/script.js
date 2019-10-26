6705a4485e245649ec84358c8d940001f117c0a7
milijana <janed3004@gmail.com>
2019-10-26 20:57:15 +0200

    scrolling to elements with jQuery added

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
 
 
-    /* For the sticky navigation */
+    /* For the sticky navigation (this is JavaScript Plug-in) */
 
   $('.js--geometrics').waypoint(function (direction) {
     if (direction == "down") {
@@ -16,4 +16,25 @@ $(document).ready(function () {
     offset: '10px;'
 
   });
+
+
+  /* Navigation scroll - performs a smooth page scroll to an anchor on the same page (This is jQuery snippet) */
+
+  $(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+
+
 });
